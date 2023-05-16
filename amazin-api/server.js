3fe44583ffdbe9d814ad156c304d84b504c5fee0
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-16 16:45:36 -0700

    Set up stripe

--- amazin-api/server.js
+++ amazin-api/server.js
@@ -1,43 +1,66 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const PORT = process.env.PORT || 8080;
-const cors = require("cors");
-const morgan = require("morgan");
+const cors = require('cors');
+const morgan = require('morgan');
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+//   apiVersion: '2022-11-15',
+// });
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://amazin-api.herokuapp.com"],
+    origin: ['http://localhost:3000', 'https://amazin-api.herokuapp.com'],
   })
 );
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// Sample GET route
-app.get("/api/data", (req, res) =>
-  res.json({
-    message: "Seems to work!",
-  })
-);
+// app.get('/config', (req, res) => {
+//   res.send({
+//     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//   });
+// });
 
-const departments = require("./routes/departments");
-const categories = require("./routes/categories");
-const products = require("./routes/products");
-const orders = require("./routes/orders");
-const reviews = require("./routes/reviews");
-const users = require("./routes/users");
+// app.get('/secret', async (req, res) => {
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: 1099,
+//       currency: 'cad',
+//       payment_method_types: ["card"],
+//     });
+//     res.json({client_secret: paymentIntent.client_secret});
+//     // res.send({
+//     //   clientSecret: paymentIntent.client_secret,
+//     // });
+//   } catch (e) {
+//     return res.status(400).send({
+//       error: {
+//         message: e.message,
+//       },
+//     });
+//   }
+  
+// });
 
-app.use("/api", departments);
-app.use("/api", categories);
-app.use("/api", products);
-app.use("/api", orders);
-app.use("/api", reviews);
-app.use("/api", users);
+const departments = require('./routes/departments');
+const categories = require('./routes/categories');
+const products = require('./routes/products');
+const orders = require('./routes/orders');
+const reviews = require('./routes/reviews');
+const users = require('./routes/users');
+
+app.use('/api', departments);
+app.use('/api', categories);
+app.use('/api', products);
+app.use('/api', orders);
+app.use('/api', reviews);
+app.use('/api', users);
 
 app.listen(PORT, () => {
   console.log(
