bceaeb388c9ae51dbc84a805aaa09ea061f56bc0
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-16 20:19:41 -0700

    Update order routes

--- amazin-api/routes/orders.js
+++ amazin-api/routes/orders.js
@@ -1,14 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   getAllOrdersByUserId,
-  getOrderById,
   createOrder,
-} = require("../controllers/orders.js");
-userId = require("../helpers/users");
+} = require('../controllers/orders.js');
+userId = require('../helpers/users');
 
-router.get("/orders", getAllOrdersByUserId);
-router.get("/orders/:orderId", getOrderById);
-router.post("/orders", createOrder);
+router.get('/users/:userId/orders', getAllOrdersByUserId);
+router.post('/users/:userId/orders', createOrder);
 
 module.exports = router;
