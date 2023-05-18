5562dcef4b8d5829a057fb94eac623ce04aa5eab
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-18 00:58:37 -0700

    Update users seeds to use bcrypt

--- amazin-api/prisma/seeds/admin.js
+++ amazin-api/prisma/seeds/admin.js
@@ -1,23 +1,26 @@
+const bcrypt = require('bcryptjs');
+hashedPassword = bcrypt.hashSync('password', 10);
+
 module.exports = [
   {
     name: 'Amazin Store',
     email: 'amazin@email.com',
-    password: 'password',
+    password: hashedPassword,
   },
 
   {
     name: 'TopSellerz',
     email: 'john@email.com',
-    password: 'password',
+    password: hashedPassword,
   },
   {
     name: 'AmazeShop',
     email: 'michael@email.com',
-    password: 'password',
+    password: hashedPassword,
   },
   {
     name: 'PrimeGoods',
     email: 'samantha@email.com',
-    password: 'password',
+    password: hashedPassword,
   },
 ];
