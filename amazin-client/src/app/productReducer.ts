11f42fda90c8fd719852febe555653d04991be39
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-25 00:09:46 -0700

    Update initial state of productReducer

--- amazin-client/src/app/productReducer.ts
+++ amazin-client/src/app/productReducer.ts
@@ -17,6 +17,7 @@ const initialState: ProductState = {
     userId: 0,
     createdAt: '',
     updatedAt: '',
+    quantityInCart: 0,
   },
   products: [],
 };
@@ -26,12 +27,27 @@ export const setCurrentProduct = (product: ProductType) => ({
   payload: product,
 });
 
+export const setProducts = (products: ProductType[]) => ({
+  type: 'SET_PRODUCTS',
+  payload: products,
+});
+
+export const updateProduct = (product: ProductType) => ({
+  type: 'UPDATE_PRODUCT',
+  payload: product,
+});
+
 export const productReducer = (
   state = initialState,
-  action: ProductAction,
+  action: ProductAction | UpdateProductAction,
 ): ProductState => {
   switch (action.type) {
     case 'SET_CURRENT_PRODUCT':
+      return {
+        ...state,
+        currentProduct: action.payload,
+      };
+      case 'UPDATE_PRODUCT':
       return {
         ...state,
         currentProduct: action.payload,
@@ -41,7 +57,32 @@ export const productReducer = (
   }
 };
 
+export const productsReducer = (
+  state = initialState,
+  action: ProductsAction,
+): ProductState => {
+  switch (action.type) {
+    case 'SET_PRODUCTS':
+      return {
+        ...state,
+        products: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 type ProductAction = {
   type: 'SET_CURRENT_PRODUCT';
   payload: ProductType;
+};
+
+type UpdateProductAction = {
+  type: 'UPDATE_PRODUCT';
+  payload: ProductType;
+};
+
+type ProductsAction = {
+  type: 'SET_PRODUCTS';
+  payload: ProductType[];
 };
