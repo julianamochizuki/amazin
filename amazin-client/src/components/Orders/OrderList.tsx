8003eadae59e4731b7cbc291a3d8e66043277657
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-06-09 02:24:33 -0700

    Udpate order message when lenght is 0

--- amazin-client/src/components/Orders/OrderList.tsx
+++ amazin-client/src/components/Orders/OrderList.tsx
@@ -6,6 +6,7 @@ import OrderListItem from './OrderListItem';
 import '../../styles/orders.css';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../app/store';
+import { Link } from 'react-router-dom';
 
 export default function OrderList() {
   const [orders, setOrders] = useState([]);
@@ -32,7 +33,16 @@ export default function OrderList() {
   return (
     <>
       <h4 className="orders-page-heading">Your Orders</h4>
-      {orderList}
+      {orders.length === 0 ? (
+        <div className="no-orders">
+          <h5>No orders found</h5>
+          <p>
+            Start shopping by <Link to="/">exploring our products</Link>.
+          </p>
+        </div>
+      ) : (
+        <>{orderList}</>
+      )}
     </>
   );
 }
